2bdba15651d91978e8b14235bec568d24ccc1e12
Munge Ryan <mungeryan@gmail.com>
2020-11-30 10:54:49 +0300

    creating a path for signup

--- award/urls.py
+++ award/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
+router.register('profile', views.ProfileViewSet)
+
 urlpatterns = [
     path('',views.index,name='index'),
     path('<username>/profile', views.user_profile, name='userprofile'),
