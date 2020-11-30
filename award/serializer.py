046b3b12f7790b90f5a71a47a7bc14d195f9364b
Munge Ryan <mungeryan@gmail.com>
2020-11-30 10:43:20 +0300

    specifying model and fields to return

--- award/serializer.py
+++ award/serializer.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import User
 from .models import Post,Profile
 
 class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
