0ef8582975e55406a45cca17539930df634c7707
Munge Ryan <mungeryan@gmail.com>
2020-11-30 10:03:52 +0300

    creating signup form class

--- views/forms.py
+++ views/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Post,Rating,Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from pyuploadcare.dj.models import ImageField
 
 class PostForm(forms.ModelForm):
     photo = ImageField(label='')
@@ -9,3 +10,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('photo', 'title', 'url', 'description', 'technologies',)
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
