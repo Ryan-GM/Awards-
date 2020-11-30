880ea421bb97336b9c3593b3fd7492955c464114
Munge Ryan <mungeryan@gmail.com>
2020-11-30 08:30:58 +0300

    defining classmethod for search project

--- award/models.py
+++ award/models.py
@@ -31,3 +31,20 @@ class Post(models.Model):
     technologies = models.CharField(max_length=200, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
