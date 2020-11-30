4ff809f0729e3208ecf91fe220b81f999cf154f0
Munge Ryan <mungeryan@gmail.com>
2020-11-30 08:56:56 +0300

    creating a classmethod for getting rates

--- award/models.py
+++ award/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime as dt 
+import datetime as dt
+from django.db.models.fields.files import ImageField 
 from django.dispatch import receiver
 from django.db.models.signals import post_save 
 # Create your models here.
@@ -11,6 +12,7 @@ class Profile(models.Model):
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
     contact = models.EmailField(max_length=100, blank=True)
+    profile_pic = models.ImageField(upload_to='images/',default='defualt.png')
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -28,6 +30,7 @@ class Post(models.Model):
     title = models.CharField(max_length=155)
     url = models.URLField(max_length=255)
     description = models.TextField(max_length=255)
+    photo = ImageField(manual_crop='1280x720')
     technologies = models.CharField(max_length=200, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     date = models.DateTimeField(auto_now_add=True, blank=True)
