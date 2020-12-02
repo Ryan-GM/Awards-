ff4a8ad33419892e0d878bae4b2ebef5c5683039
Munge Ryan <mungeryan@gmail.com>
2020-12-02 15:06:45 +0300

    installing a sw

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,12 +7,3 @@ if('serviceWorker' in navigator) {
     });
 });
 }
-self.addEventListener('install',function(event){
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-          })
-      );
-});
